cca93f92ec606b28039b54863e0af3d8ecdeada5
Vladislav Samura <vx5080@yandex.ru>
2024-07-17 13:21:21 +0900

    Nav meny is right

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+let opt = {
+    edge: 'right',
+    draggable: true,
+    inDuration: 250,
+    outDuration: 200,
+    onOpenStart: null,
+    onOpenEnd: null,
+    onCloseStart: null,
+    onCloseEnd: null,
+    preventScrolling: false
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    var instances = M.Sidenav.init(elems, opt);
   });
